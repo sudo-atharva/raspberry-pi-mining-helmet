68829c7ae090eda4e95c54556cd5a01d28668869
sudo-atharva <atharva.makxenia@gmail.com>
2025-08-29 11:20:03 +0530

    new arduino_code_for_hc12

--- arduino_code_for-hc12/arduino_code_for-hc12.ino
+++ arduino_code_for-hc12/arduino_code_for-hc12.ino
@@ -1,30 +1,17 @@
 #include <SoftwareSerial.h>
-
-// HC-12 connections
-#define HC12_TX 10   // Arduino pin → HC-12 RX
-#define HC12_RX 11   // Arduino pin ← HC-12 TX
-
-SoftwareSerial HC12(HC12_TX, HC12_RX);
+SoftwareSerial HC12(10, 11);
 
 void setup() {
-  Serial.begin(9600);   // Serial to computer
-  HC12.begin(9600);     // Serial to HC-12
+  Serial.begin(9600);
+  HC12.begin(9600);
+  Serial.println("HC-12 Test Ready");
 }
 
 void loop() {
-  // Forward everything from HC-12 → Serial
-  if (HC12.available()) {
-    while (HC12.available()) {
-      char c = HC12.read();
-      Serial.write(c);
-    }
+  while (HC12.available()) {
+    Serial.write(HC12.read());  // Show received data
   }
-
-  // Forward everything from Serial → HC-12
-  if (Serial.available()) {
-    while (Serial.available()) {
-      char c = Serial.read();
-      HC12.write(c);
-    }
+  while (Serial.available()) {
+    HC12.write(Serial.read());  // Send typed data
   }
 }
